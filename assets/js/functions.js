7a2a2800b279058d4470b8a9d19addad77973ab1
Ioannis Tzortzakis <ioannis.tzortzak@gmail.com>
2023-06-15 13:23:22 +0300

    Email() doesn't work yet

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,3 +7,16 @@ function downloadPDF() {
     link.click();
     document.body.removeChild(link);
   }
+
+
+  function sendEmail() {
+    // AJAX request to the PHP file
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert(this.responseText);
+        }
+    };
+    xhttp.open("GET", "send_email.php", true);
+    xhttp.send();
+}
