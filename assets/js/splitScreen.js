8d176aa1546e22406d9571c70c206179b1a8e431
Yiannis Tzortzakis <103953727+ytzortz@users.noreply.github.com>
2026-09-09 03:24:48 +0300

    tetris themes added

--- assets/js/splitScreen.js
+++ assets/js/splitScreen.js
@@ -19,6 +19,7 @@ function startGame() {
         <p><canvas id="upcoming"></canvas></p>
         <p>score <span id="score">00000</span></p>
         <p>rows <span id="rows">0</span></p>
+        <p><i id="soundToggle" class="bi bi-volume-mute-fill" onclick="toggleGameSound();"></i></p>
       </div>
       <canvas id="canvas">
         Sorry, this example cannot be run because your browser does not support the &lt;canvas&gt; element
@@ -45,4 +46,69 @@ function turnOffGame(){
     document.getElementById('containerHidden').style.height = "0px";
     document.getElementById("thankYouNote_wrapper").style.visibility = "visible";
 
+    stopGameSound();
+
+}
+
+// ------------------------------
+// Tetris game soundtrack
+// ------------------------------
+
+var gameTracks = [
+    "assets/mp3/tetris_theme_a.mp3",
+    "assets/mp3/tetris_theme_b.mp3",
+    "assets/mp3/tetris_theme_c.mp3"
+];
+var gameAudio = null;
+var gameTrackIndex = 0;
+var gameTrackGapTimeout = null;
+var gameSoundOn = false;
+
+function playNextGameTrack() {
+    if (!gameAudio) {
+        gameAudio = new Audio();
+        gameAudio.volume = 0.6;
+        gameAudio.addEventListener('ended', function() {
+            gameTrackGapTimeout = setTimeout(function() {
+                gameTrackIndex = (gameTrackIndex + 1) % gameTracks.length;
+                playNextGameTrack();
+            }, 500);
+        });
+    }
+
+    gameAudio.src = gameTracks[gameTrackIndex];
+    gameAudio.play();
+}
+
+function toggleGameSound() {
+    gameSoundOn = !gameSoundOn;
+
+    var icon = document.getElementById("soundToggle");
+
+    if (gameSoundOn) {
+        icon.classList.remove("bi-volume-mute-fill");
+        icon.classList.add("bi-volume-up-fill");
+        playNextGameTrack();
+    } else {
+        icon.classList.remove("bi-volume-up-fill");
+        icon.classList.add("bi-volume-mute-fill");
+        stopGameSound();
+    }
+}
+
+function stopGameSound() {
+    clearTimeout(gameTrackGapTimeout);
+
+    if (gameAudio) {
+        gameAudio.pause();
+        gameAudio.currentTime = 0;
+    }
+
+    gameSoundOn = false;
+
+    var icon = document.getElementById("soundToggle");
+    if (icon) {
+        icon.classList.remove("bi-volume-up-fill");
+        icon.classList.add("bi-volume-mute-fill");
+    }
 }
